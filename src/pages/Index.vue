8ca3a36a8ada927d7a202850c62e407cf42de97d
Max Silva <maxleandro2@gmail.com>
2018-12-03 10:50:00 -0200

    subindo Arquivos

---
+++ src/pages/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page class="flex flex-center">
+    <vue-audio :file="path"></vue-audio>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+import boot from 'bootstrap-css'
+import VueAudio from 'vue-audio'
+export default {
+  name: 'PageIndex',
+  components: {
+    VueAudio, boot
+  },
+  data () {
+    return {
+      path: './statics/musica.mp3'
+    }
+  },
+  methods: {
+  }
+}
+</script>
